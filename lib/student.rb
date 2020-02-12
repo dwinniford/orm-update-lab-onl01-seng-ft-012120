18f45f1a414f973b2c8e6c55e7f0fbc44dbf3ecc
Dave Winniford <dwinniford@gmail.com>
2020-02-12 19:18:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,6 +44,11 @@ class Student
   def self.new_from_db(row)
     Student.new(row[0], row[1], row[2])
   end 
+  
+  def self.find_by_name(name)
+    row = DB[:conn].execute("SELECT * FROM students WHERE name = #{name}")[0]
+    self.new_from_db(row)
+  end 
 
 
 end
